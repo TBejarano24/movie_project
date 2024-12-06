4bb78b4af1146c93b002e7060cc1de7c9bdf64a6
TBejarano24 <bejaranotomas04@gmail.com>
2024-12-06 20:55:52 -0300

    Added yet another API URL containing the movies genres IDs, and added a function that allows me to decipher each movie genres and upload them altogether to the database.

--- Scripts/API.py
+++ Scripts/API.py
@@ -3,6 +3,20 @@ import pyodbc
 import os
 from dotenv import load_dotenv
 
+def decipher_genre(movie_genres, genres_data, array):
+    
+    for i in movie_genres:
+        
+        for genre in genres_data['genres']:
+            
+            if genre['id'] == i:
+                
+                array.append(genre['name'])
+            
+            else:
+                
+                pass
+
 def calculate_popularity(popularity, max_popularity):
     
     if 0 <= popularity <= max_popularity / 5:
@@ -32,16 +46,23 @@ headers = {
     "Authorization": access_token
 }
 
-for page in range(1, 5):
 
-    url = f"https://api.themoviedb.org/3/movie/popular?language=en-US&page={page}"
+url_genres = 'https://api.themoviedb.org/3/genre/movie/list?language=en'
 
-    response = requests.get(url, headers=headers)
-    data = response.json()
+response_genres = requests.get(url_genres, headers=headers)
+data_genres = response_genres.json()
+
+
+for page in range(1, 6):
+
+    url_movies = f"https://api.themoviedb.org/3/movie/popular?language=en-US&page={page}"
+
+    response_movies = requests.get(url_movies, headers=headers)
+    data_movies = response_movies.json()
 
     if page == 1:
         
-        most_popular = data['results'][0]['popularity']
+        most_popular = data_movies['results'][0]['popularity']
 
     conn = pyodbc.connect(f'DRIVER={driver};SERVER=.\SQLEXPRESS;DATABASE=movie_db;UID={uid};PWD={pwd}')
 
@@ -56,7 +77,7 @@ for page in range(1, 5):
     query2 = """
     IF NOT EXISTS(SELECT 1 FROM Movies WHERE title = ?)
     BEGIN
-        INSERT INTO Movies(title, release_date, original_language, vote_average, vote_count, popularity, overview, genreIDs)
+        INSERT INTO Movies(title, release_date, original_language, vote_average, vote_count, popularity, overview, genres)
         VALUES(?, ?, ?, ?, ?, ?, ?, ?)
 
         INSERT INTO Movie_Popularity(movieID, title, popularity_category)
@@ -67,7 +88,7 @@ for page in range(1, 5):
         IF (SELECT popularity FROM Movies WHERE title = ?) != ?
         BEGIN
             UPDATE Movies
-            SET title = ?, release_date = ?, original_language = ?, vote_average = ?, vote_count = ?, popularity = ?, overview = ?, genreIDs = ?
+            SET title = ?, release_date = ?, original_language = ?, vote_average = ?, vote_count = ?, popularity = ?, overview = ?, genres = ?
             
             UPDATE Movie_Popularity
             SET title = ?, popularity_category = ?
@@ -75,17 +96,19 @@ for page in range(1, 5):
     END
     """
 
-    index = len(data['results'])
+    index = len(data_movies['results'])
 
     for i in range(0, index):
         
+        genres = []
+        decipher_genre(data_movies['results'][i]['genre_ids'], data_genres, genres)
         
-        cursor.execute(query2, data['results'][i]['title'],
-                    data['results'][i]['title'], data['results'][i]['release_date'], data['results'][i]['original_language'], data['results'][i]['vote_average'], data['results'][i]['vote_count'], data['results'][i]['popularity'], data['results'][i]['overview'], str(data['results'][i]['genre_ids']),
-                    data['results'][i]['title'], calculate_popularity(data['results'][i]['popularity'], most_popular),
-                    data['results'][i]['title'], data['results'][i]['popularity'], data['results'][i]['title'], data['results'][i]['release_date'], data['results'][i]['original_language'], data['results'][i]['vote_average'], data['results'][i]['vote_count'], data['results'][i]['popularity'], data['results'][i]['overview'], str(data['results'][i]['genre_ids']), data['results'][i]['title'], calculate_popularity(data['results'][i]['popularity'], most_popular))
+        cursor.execute(query2, data_movies['results'][i]['title'],
+                    data_movies['results'][i]['title'], data_movies['results'][i]['release_date'], data_movies['results'][i]['original_language'], data_movies['results'][i]['vote_average'], data_movies['results'][i]['vote_count'], data_movies['results'][i]['popularity'], data_movies['results'][i]['overview'], str(genres),
+                    data_movies['results'][i]['title'], calculate_popularity(data_movies['results'][i]['popularity'], most_popular),
+                    data_movies['results'][i]['title'], data_movies['results'][i]['popularity'], data_movies['results'][i]['title'], data_movies['results'][i]['release_date'], data_movies['results'][i]['original_language'], data_movies['results'][i]['vote_average'], data_movies['results'][i]['vote_count'], data_movies['results'][i]['popularity'], data_movies['results'][i]['overview'], str(genres), data_movies['results'][i]['title'], calculate_popularity(data_movies['results'][i]['popularity'], most_popular))
         
-        print(f"Checking data for {data['results'][i]['title']}...")
+        print(f"Checking data for {data_movies['results'][i]['title']}...")
 
     conn.commit()
     conn.close()
