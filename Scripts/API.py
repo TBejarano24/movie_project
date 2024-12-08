22e9638ee673fb3be7d82d3004af8d2461e13c75
TBejarano24 <bejaranotomas04@gmail.com>
2024-12-08 01:32:13 -0300

    CSV conversion and MERGE statement Added pandas and pyodbc libraries to select a table's content, convert it to a CSV file and store it locally. I've also radically changed the SQL Query into a MERGE Statement in order to keep a better control of data updates in the table. To do this I also added a new auxiliar table that receives the updated data and transfers it to the original. I keep receiving ERROR 10713 though, still working on that...

--- Scripts/API.py
+++ Scripts/API.py
@@ -1,7 +1,42 @@
 import requests
 import pyodbc
 import os
+import pandas as pd
 from dotenv import load_dotenv
+
+def import_as_csv (driver, server, database, table, user, password, output_file):
+    
+    try:
+    
+        print(f'Establishing connection with {server}...')
+        
+        sql_conn = pyodbc.connect(f'DRIVER={{{driver}}}; SERVER={server};DATABASE={database};UID={user};PWD={password}')
+        
+        print('Connection successfully established')
+        
+        print(f'Retrieving data from {table}...')
+        
+        #se toman todos los datos de la tabla en la base de datos
+        query = f"SELECT * FROM {table}"
+        
+        #se ejecuta la consulta y los datos se convierten en dataframe
+        df = pd.read_sql(query, con=sql_conn)
+        
+        print('Importing as CSV...')
+        
+        #se convierte el dataframe en archivo CSV y se guarda en la
+        #ruta y formato especificados
+        df.to_csv(output_file, index=False, encoding='utf-8')
+        
+        print('File imported succesfully!')
+        
+    except Exception as e:
+        print(f'Error: {e}')
+
+
+    finally:
+        #se cierra la conexión
+        sql_conn.close()
 
 def decipher_genre(movie_genres, genres_data, array):
     
@@ -39,6 +74,7 @@ access_token = os.getenv('ACCESS_TOKEN')
 driver = os.getenv('DRIVER')
 uid = os.getenv('DB_USER')
 pwd = os.getenv('DB_PWD')
+csv_path = os.getenv('FILE_PATH')
 
 
 headers = {
@@ -69,8 +105,7 @@ for page in range(1, 6):
     cursor = conn.cursor()
 
     # query1 = f"""
-    # CREATE TABLE #Top_Movies (title varchar(50))
-    # INSERT INTO #Top_Movies VALUES({i})
+    
     # """
 
 
@@ -85,14 +120,34 @@ for page in range(1, 6):
     END
     ELSE
     BEGIN
-        IF (SELECT popularity FROM Movies WHERE title = ?) != ?
-        BEGIN
-            UPDATE Movies
-            SET title = ?, release_date = ?, original_language = ?, vote_average = ?, vote_count = ?, popularity = ?, overview = ?, genres = ?
-            
-            UPDATE Movie_Popularity
-            SET title = ?, popularity_category = ?
-        END
+        MERGE Updated_Movie_Data AS TARGET
+        USING Movies AS SOURCE
+    
+            ON (TARGET.movieID = SOURCE.movieID)
+        WHEN MATCHED
+            AND TARGET.title <> SOURCE.title
+            OR TARGET.vote_average <> SOURCE.vote_average
+            OR TARGET.vote_count <> SOURCE.vote_count
+            OR TARGET.popularity <> SOURCE.popularity
+        THEN
+            UPDATE
+                SET TARGET.title = SOURCE.title
+                SET TARGET.release_date = SOURCE.release_date
+                SET TARGET.original_language = SOURCE.original_language
+                SET TARGET.vote_average = SOURCE.vote_average
+                SET TARGET.vote_count = SOURCE.vote_count
+                SET TARGET.popularity = SOURCE.popularity
+                SET TARGET.overview = SOURCE.overview
+                SET TARGET.genres = SOURCE.genres
+        
+        WHEN NOT MATCHED BY TARGET
+            THEN 
+                INSERT(title, release_date, original_language, vote_average, vote_count, popularity, overview, genres)
+                VALUES(SOURCE.title, SOURCE.release_date, SOURCE.original_language, SOURCE.vote_average, SOURCE.vote_count, SOURCE.popularity, SOURCE.overview, SOURCE.genres)
+    
+        WHEN NOT MATCHED BY SOURCE
+            THEN DELETE;
+        SELECT 1;
     END
     """
 
@@ -105,8 +160,7 @@ for page in range(1, 6):
         
         cursor.execute(query2, data_movies['results'][i]['title'],
                     data_movies['results'][i]['title'], data_movies['results'][i]['release_date'], data_movies['results'][i]['original_language'], data_movies['results'][i]['vote_average'], data_movies['results'][i]['vote_count'], data_movies['results'][i]['popularity'], data_movies['results'][i]['overview'], str(genres),
-                    data_movies['results'][i]['title'], calculate_popularity(data_movies['results'][i]['popularity'], most_popular),
-                    data_movies['results'][i]['title'], data_movies['results'][i]['popularity'], data_movies['results'][i]['title'], data_movies['results'][i]['release_date'], data_movies['results'][i]['original_language'], data_movies['results'][i]['vote_average'], data_movies['results'][i]['vote_count'], data_movies['results'][i]['popularity'], data_movies['results'][i]['overview'], str(genres), data_movies['results'][i]['title'], calculate_popularity(data_movies['results'][i]['popularity'], most_popular))
+                    data_movies['results'][i]['title'], calculate_popularity(data_movies['results'][i]['popularity'], most_popular))
         
         print(f"Checking data for {data_movies['results'][i]['title']}...")
 
@@ -114,6 +168,9 @@ for page in range(1, 6):
     conn.close()
 
     print(f"Page {page} finished")
+    
+
+import_as_csv(driver, '.\SQLEXPRESS', 'movie_db', 'Movies', uid, pwd, csv_path)
 
 # print(top_movies_list)
 
